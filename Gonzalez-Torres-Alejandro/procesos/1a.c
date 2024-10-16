aa5398fc6c06b176cd7832b03ad0a069008f4d07
AlejandroCic <agonzalezt2024@cic.ipn.mx>
2024-10-16 15:16:54 -0600

    Update 1a.c

--- Gonzalez-Torres-Alejandro/procesos/1a.c
+++ Gonzalez-Torres-Alejandro/procesos/1a.c
@@ -1,1 +1,15 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    // Mostrar el número de argumentos pasados, excluyendo el nombre del programa
+    std::cout << "argc = " << argc - 1 << std::endl;
+
+    // Si no hay argumentos adicionales, termina con código de salida 0
+    if (argc == 1)
+        return 0;
+
+    // Devolver el número de argumentos totales
+    return argc - 1;
+}
 
