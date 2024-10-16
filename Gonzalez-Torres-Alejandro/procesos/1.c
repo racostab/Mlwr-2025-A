a058e5bdb45ec1caf880db8eaa0502388bb8f41b
AlejandroCic <agonzalezt2024@cic.ipn.mx>
2024-10-16 15:16:07 -0600

    Update 1.c

--- Gonzalez-Torres-Alejandro/procesos/1.c
+++ Gonzalez-Torres-Alejandro/procesos/1.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    // Mostrar el número de argumentos pasados, excluyendo el nombre del programa
+    printf("argc = %d\n", argc - 1);
+
+    // Si no hay argumentos adicionales, termina con código de salida 0
+    if (argc == 1)
+        return 0;
+
+    // Devolver el número de argumentos totales
+    return argc - 1;
+}
 
