6975934a4c84e1e75017450872e6121c3ce40ca3
Jose Alexis Terrazas Chavez <121589051+Alexis3445@users.noreply.github.com>
2024-10-29 20:18:36 -0600

    practica_1.cpp

--- Terrazas-Chavez-Jose-Alexis/Practicas/C++/practica_1.cpp
+++ Terrazas-Chavez-Jose-Alexis/Practicas/C++/practica_1.cpp
@@ -1,1 +1,15 @@
+/*
+Programa que recibe argumentos y muestra cuantos son
+*/
+#include <iostream>
+using namespace std;
 
+int main(int argc, char* argv[]) {
+    cout << "argc = " << argc << endl;
+
+    if (argc == 1) {
+        return 0;
+    } else {
+        return argc;
+    }
+}
